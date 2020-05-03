88113aeb4facb45de137f1818826e120f7d85de8
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-05-03 23:42:17 +0800

    2nd commit :1st completed version of momentumJS

--- greeting.js
+++ greeting.js
@@ -1,8 +1,12 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+  greeting = document.querySelector(".js-greetings"),
+  toDosForm = document.querySelector(".js-toDoForm"),
+  toDosList = document.querySelector(".js-toDoList");
+
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
+HIDING_CN = "hiding";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
@@ -13,24 +17,29 @@ function handleSubmit(event) {
   const currentValue = input.value;
   paintGreeting(currentValue);
   saveName(currentValue);
+  toDosForm.classList.remove(HIDING_CN);
+  toDosList.classList.remove(HIDING_CN);
 }
 
 function askForName() {
-  form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
+  form.classList.add(HIDING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}!`;
+  greeting.innerText = `Welcome back, ${text}!`;
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
+    toDosForm.classList.add(HIDING_CN);
+    toDosList.classList.add(HIDING_CN);
     askForName();
   } else {
+    toDosForm.classList.remove(HIDING_CN);
+    toDosList.classList.remove(HIDING_CN);
     paintGreeting(currentUser);
   }
 }
